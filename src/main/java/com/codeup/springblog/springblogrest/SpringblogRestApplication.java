c68b622be8c9dac5944a17141491f78b9ddfcc45
Jay Arredondo <jearredondo13@gmail.com>
2023-01-12 09:53:51 -0600

    adding project structure

---
+++ src/main/java/com/codeup/springblog/springblogrest/SpringblogRestApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.springblog.springblogrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringblogRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringblogRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/springblog/springblogrest/SpringblogRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.springblog.springblogrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringblogRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
